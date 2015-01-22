9cab2285e90f2e02fd203bfda84d53bd8125b2ce
ryomtmt <ryo@ivy.local>
2015-01-23 01:01:34 +0900

    add git dependent packages

--- git_install.sh
+++ git_install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 yum -y remove git
+yum -y install curl-devel expat-devel gettext-devel openssl-devel zlib-devel
+
+cd ~
 wget https://www.kernel.org/pub/software/scm/git/git-2.2.0.tar.gz
 tar zxf git-2.2.0.tar.gz
 cd git-2.2.0
