ba29c4419d4546506318d968e52f4410f029665a
ryomtmt <ryo@ivy.local>
2020-06-21 01:28:52 +0900

    全面的に書き直し

--- ffmpeg_setup.sh
+++ ffmpeg_setup.sh
@@ -1,48 +1,115 @@
-sudo yum install -y autoconf automake gcc gcc-c++ libtool make nasm pkgconfig zlib-devel
+#!/bin/bash
 
-mkdir -p ~/ffmpeg/sources
+sudo yum install -y autoconf automake gcc gcc-c++ libtool make nasm zlib-devel \
+libass libass-devel cmake freetype freetype-devel gnutls gnutls-devel SDL SDL-devel \
+SDL2 SDL2-devel openssl openssl-devel libva libva-devel libvdpau libvorbis libxcb libxcb-devel nasm \
+libvmaf libvmaf-devel libva libva-devel libvarlink-devel libvarlink-util
 
-cd ~/ffmpeg/sources
-git clone git://github.com/yasm/yasm.git
-cd yasm
-autoreconf -fiv
-./configure --prefix="$HOME/ffmpeg/build" --bindir="$HOME/ffmpeg/bin"
+if $(rpm -q yasm &>/dev/null) ; then
+  :
+else
+  sudo yum install -y https://cbs.centos.org/kojifiles/packages/yasm/1.3.0/10.el8/x86_64/yasm-1.3.0-10.el8.x86_64.rpm
+fi
+if $(rpm -q yasm-devel &>/dev/null) ; then
+  :
+else
+  sudo yum install -y https://cbs.centos.org/kojifiles/packages/yasm/1.3.0/10.el8/x86_64/yasm-devel-1.3.0-10.el8.x86_64.rpm
+fi
+
+mkdir -p ${HOME}/ffmpeg/sources ${HOME}/ffmpeg/build ${HOME}/bin
+
+FFMPEG_BUILD="${HOME}/ffmpeg/build"
+FFMPEG_SOURCES="${HOME}/ffmpeg/sources"
+FFMPEG_VERSION="n4.3"
+FDK_VERSION="v2.0.1"
+
+#cd ~/ffmpeg/sources
+#git clone https://github.com/yasm/yasm.git
+#cd yasm
+#autoreconf -fiv
+#./configure --prefix="${HOME}/ffmpeg/build" --bindir="${HOME}/bin"
+#make
+#make install
+#make distclean
+
+#libx264
+cd ${FFMPEG_SOURCES} && \
+git clone https://code.videolan.org/videolan/x264.git && \
+cd x264 && \
+PATH="${HOME}/bin:${PATH}" PKG_CONFIG_PATH="${FFMPEG_BUILD}/lib/pkgconfig" ./configure \
+--prefix="${FFMPEG_BUILD}" --bindir="${FFMPEG_BUILD}/bin" --enable-static --enable-pic && \
+PATH="${HOME}/bin:${PATH}" make && \
+make install
+make distclean
+
+#libx265
+sudo yum install -y numad numactl numactl-devel mercurial && \
+cd ${FFMPEG_SOURCES} && \
+if cd x265 2> /dev/null; then
+  hg pull && hg update && cd ..
+else
+  hg clone https://bitbucket.org/multicoreware/x265
+fi && \
+cd x265/build/linux && \
+PATH="${HOME}/bin:${PATH}" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="${FFMPEG_BUILD}" -DENABLE_SHARED:bool=off ../../source && \
+PATH="${HOME}/bin:${PATH}" make && \
+make install
+make clean
+
+#libvpx インストール
+cd ${FFMPEG_SOURCES} && \
+git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git && \
+cd libvpx && \
+PATH="${HOME}/bin:${PATH}" ./configure --prefix="${FFMPEG_BUILD}" --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm && \
+PATH="${HOME}/bin:${PATH}" make && \
+make install
+make clean
+
+#libaom インストール
+cd ${FFMPEG_SOURCES} && \
+git clone --depth 1 https://aomedia.googlesource.com/aom && \
+mkdir -p aom_build && \
+cd aom_build && \
+PATH="${HOME}/bin:${PATH}" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="${FFMPEG_BUILD}" -DENABLE_SHARED=off -DENABLE_NASM=on ../aom && \
+PATH="${HOME}/bin:${PATH}" make && \
+make install
+make clean
+
+#fdk-aac
+cd ${FFMPEG_SOURCES} && \
+git clone https://git.code.sf.net/p/opencore-amr/fdk-aac && \
+cd fdk-aac && \
+git checkout -b ${FDK_VERSION} ${FDK_VERSION} && \
+autoreconf -fiv && \
+./configure --prefix="${FFMPEG_BUILD}" --disable-shared && \
 make
 make install
 make distclean
 
-cd ~/ffmpeg/sources
-git clone git://git.videolan.org/x264
-cd x264
-./configure --prefix="$HOME/ffmpeg/build" --bindir="$HOME/ffmpeg/bin" --enable-static
-make
+#lame-mp3
+cd ${FFMPEG_SOURCES} && \
+curl -L -O https://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz && \
+tar xzvf lame-3.100.tar.gz && \
+cd lame-3.100 && \
+PATH="${HOME}/bin:${PATH}" ./configure --prefix="${FFMPEG_BUILD}" --bindir="${FFMPEG_BUILD}/bin" --disable-shared --enable-nasm && \
+PATH="${HOME}/bin:${PATH}" make && \
 make install
-make distclean
+make clean
 
-cd ~/ffmpeg/sources
-git clone git://git.code.sf.net/p/opencore-amr/fdk-aac
-cd fdk-aac
-autoreconf -fiv
-./configure --prefix="$HOME/ffmpeg/build" --disable-shared
-make
-make install
-make distclean
-
-cd ~/ffmpeg/sources
-curl -L -O http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
-tar xzvf lame-3.99.5.tar.gz
-cd lame-3.99.5
-./configure --prefix="$HOME/ffmpeg/build" --bindir="$HOME/ffmpeg/bin" --disable-shared --enable-nasm
-make
-make install
-make distclean
-
-cd ~/ffmpeg/sources
-git clone git://source.ffmpeg.org/ffmpeg
-cd ffmpeg
-git checkout -b tag2.5.3 n2.5.3
-PKG_CONFIG_PATH="$HOME/ffmpeg/build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg/build" --extra-cflags="-I$HOME/ffmpeg/build/include" --extra-ldflags="-L$HOME/ffmpeg/build/lib" --bindir="$HOME/ffmpeg/bin" --enable-gpl --enable-nonfree --enable-libfdk_aac --enable-libmp3lame --enable-libx264
-make
+#ffmpeg
+cd ${FFMPEG_SOURCES} && \
+git clone https://github.com/FFmpeg/FFmpeg.git ffmpeg && \
+cd ffmpeg && \
+git checkout -b ${FFMPEG_VERSION} ${FFMPEG_VERSION} && \
+PATH="${HOME}/bin:${PATH}" PKG_CONFIG_PATH="${FFMPEG_BUILD}/lib/pkgconfig" ./configure \
+  --prefix="${FFMPEG_BUILD}" \
+  --pkg-config-flags="--static" \
+  --extra-cflags="-I${FFMPEG_BUILD}/include" --extra-ldflags="-L${FFMPEG_BUILD}/lib" --extra-libs="-lpthread -lm" \
+  --bindir="${HOME}/bin" \
+  --enable-openssl --enable-gpl --enable-nonfree --enable-version3 --enable-libass --enable-libfreetype \
+  --enable-libfdk_aac --enable-libmp3lame --enable-libx264 --enable-libx265 --enable-libvpx \
+  --enable-static --enable-libvmaf && \
+PATH="${HOME}/bin:${PATH}" make && \
 make install
 make distclean
 hash -r
